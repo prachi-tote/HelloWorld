1482bff5312b30e94fab7fd53b14e25220b07bb3
Prachi tote <prachi.tote77@gmail.com>
2023-04-06 17:12:29 +0530

    [Prachi] Adding human program with instace members

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
+using System.Text;
+using System.Threading.Tasks;
 namespace HelloWorld
 {
     class Program
@@ -11,6 +12,16 @@ namespace HelloWorld
             Console.WriteLine("Welcome");
 
             Console.WriteLine("Modifying changes in Remote");
+            Human human = new Human();
+            Console.WriteLine("Please enter human name");
+            human.name = Console.ReadLine();
+            Console.WriteLine("please enter human age");
+            human.age = Convert.ToInt32(Console.ReadLine());
+            human.weight = Convert.ToSingle(Console.ReadLine());
+            human.height = Convert.ToSingle(Console.ReadLine());
+            human.Speak();
+            human.HumanDetails(human.name, human.age, human.height, human.weight);
+            Console.ReadLine();
         }
 
     }
---
+++ HelloWorld/human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelloWorld
+{
+    public class Human
+    {
+        public string name;
+        public int age;
+        public float weight, height;
+        public void Speak()
+        {
+            Console.WriteLine("Human can speak");
+        }
+        public void HumanDetails(String humanname, int age, float weight, float height)
+        {
+            Console.WriteLine("HumanDetails:{0},{1},{2},{3}", humanname, age, weight, height);
+        }
+    }
+}
