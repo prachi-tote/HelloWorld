4f428f5e80044dee3410acf6bdbbd373e8b0e6ac
Prachi tote <prachi.tote77@gmail.com>
2023-04-06 10:21:19 +0530

    changes in local repo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,18 +3,15 @@ using System;
 namespace HelloWorld
 {
     class Program
-    {  
-        static void Main(string[] args)
     {
-        Console.WriteLine("Adding local repo");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome");
-<<<<<<< HEAD
-        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding local repo");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome");
+
+            Console.WriteLine("Modifying changes in Remote");
         }
-=======
-        Console.WriteLine("Modifying changes in Remote");
+
     }
->>>>>>> eecd8c5bec42b9518eb737b20d122aa22c1f0734
-}
 }
